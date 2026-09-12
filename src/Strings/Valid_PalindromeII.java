821dc73feb98e3cbb22d26939df961c221554843
RatanGupta <gratan536@gmail.com>
2026-09-13 00:54:19 +0530

    Add ValidPalindromeII class to determine if a string can be a palindrome after deleting at most one character

---
+++ src/Strings/Valid_PalindromeII.java
@@ -0,0 +1,55 @@
+package Strings;
+/*Given a string s, return true if the s can be palindrome after deleting at most one character from it.
+
+
+
+Example 1:
+
+Input: s = "aba"
+Output: true
+Example 2:
+
+Input: s = "abca"
+Output: true
+Explanation: You could delete the character 'c'.
+Example 3:
+
+Input: s = "abc"
+Output: false
+
+
+ */
+public class Valid_PalindromeII {
+
+    public static void main(String[] args) {
+        Valid_PalindromeII valid_palindromeII = new Valid_PalindromeII();
+        String s = "abca";
+        boolean result = valid_palindromeII.validPalindrome(s);
+        System.out.println(result); // Output: true
+    }
+
+    public boolean validPalindrome(String s) {
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return isPalindrome(s, left + 1, right) || isPalindrome(s, left, right - 1);
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+    private boolean isPalindrome(String s, int left, int right) {
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
